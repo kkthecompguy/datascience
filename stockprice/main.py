d88ba76e9a2c9542767323983439aaf5dffd5e0f
kkthecompguy <hnkhosamomollo3@gmail.com>
2022-03-09 14:27:01 +0300

    initial commit

---
+++ stockprice/main.py
@@ -0,0 +1,29 @@
+import yfinance as yf
+import streamlit as st
+import pandas as pd
+
+st.write("""
+# Simple Stock Price App 
+
+Show are the stock closing price and volume of Apple
+
+""")
+
+#define the ticker symbol
+tickerSymbol = 'AAPL'
+#get data on this ticker
+tickerData = yf.Ticker(tickerSymbol)
+
+#get the historical prices for this ticker
+tickerDf = tickerData.history(period="1d", start="2010-05-31", end="2021-5-31")
+
+# Open	High	Low	Close	Volume	Dividends	Stock Splits
+
+st.write("""
+## Closing Stock Price
+""")
+st.line_chart(tickerDf.Close)
+st.write("""
+## Volume Price
+""")
+st.line_chart(tickerDf.Volume)
